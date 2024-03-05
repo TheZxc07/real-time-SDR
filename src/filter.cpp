8f9030ef82eb0ce5432e3a30a6fbb5c63848087c
Amr Elhossan <thehorse2003@gmail.com>
2024-03-05 16:25:25 -0500

    BPF setup for stereo some stero work aswell

--- src/filter.cpp
+++ src/filter.cpp
@@ -28,6 +28,25 @@ void impulseResponseLPF(float Fs, float Fc, unsigned short int num_taps, std::ve
 	}
 }
 
+void impulseResponseBPF(float Fs, float[] Fb, unsigned short int num_taps, std::vector<float> &h)
+{
+	h.clear(); h.resize(num_taps, 0.0);
+	
+	float normalized_center = ((Fb[1] + Fb[0])/2)/(Fs/2);
+	float normalized_pass = ((Fb[1] - Fb[0])/2)/(Fs/2);
+	
+	for (int i = 0; i < num_taps; i++){
+		if (i == (num_taps-1.0)/2.0){
+			h[i] = normalized_pass;
+		} else {
+			h[i] = normalized_pass*((sin(PI*(normalized_pass/2)*(i-(num_taps-1)/2)))/(PI*(normalized_pass/2)*(i-(num_taps-1)/2)));
+		}
+		h[i] = h[i]*cos(i*PI*normalized_center);
+		h[i] = h[i]*sin(i*PI/((float)num_taps))*sin(i*PI/((float)num_taps));
+	}
+}
+
+
 // function to compute the filtered output "y" by doing the convolution
 // of the input data "x" with the impulse response "h"
 void convolveFIR(std::vector<float> &y, const std::vector<float> &x, const std::vector<float> &h, std::vector<float> &initial_state, const int decimation_factor)
@@ -39,22 +58,6 @@ void convolveFIR(std::vector<float> &y, const std::vector<float> &x, const std::
 				y[n/decimation_factor] += h[k]*initial_state[n-k+initial_state.size()];
 			}else{
 				y[n/decimation_factor] += h[k]*x[n-k];
-			}
-		}
-	}
-
-	initial_state = std::vector<float>(x.end()-h.size()+1,x.end());
-	
-}
-void convolveFIR(const int over, std::vector<float> &y, const std::vector<float> &x, const std::vector<float> &h, std::vector<float> &initial_state, const int decimation_factor)
-{
-	y.clear(); y.resize(x.size()/decimation_factor);
-	for (int n = 0; n < x.size(); n+=decimation_factor) {
-		for (int k = 0; k < h.size(); k++){
-			if (n-k < 0) {
-				y[n/decimation_factor] += (short int)((16384*h[k]*initial_state[n-k+initial_state.size()]));
-			}else{
-				y[n/decimation_factor] += (short int)((16384*h[k]*x[n-k]));
 			}
 		}
 	}
--- src/stereo.cpp
+++ src/stereo.cpp
@@ -1,5 +1,22 @@
 #include "stereo.h"
-void stereo_mode0(){std::cerr << "TODO! - To be implemented in future renditions.\n";}
+void stereo_mode0(){
+	
+	
+	while(true){
+	
+	std::cin.read(reinterpret_cast<char*>(IQ_buf.data()), 2*block_size);
+
+		while(sample_num < 2*block_size){
+			iq_sample = ((float)IQ_buf[sample_num]-128.0)/128.0;
+			IQ_index = sample_num & 0x01;
+			(*IQ[IQ_index])[sample_num >> 1] = iq_sample;
+			sample_num += 1;
+		}
+	sample_num = 0;
+	
+	}
+	//std::cerr << "TODO! - To be implemented in future renditions.\n";
+}
 
 void stereo_mode1(){std::cerr << "TODO! - To be implemented in future renditions.\n";}
 
