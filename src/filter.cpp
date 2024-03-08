d73d58e4a0a808f14c8e01ffefc283ba1a844fa6
Hydar-Zartash <zartashh@mcmaster.ca>
2024-03-07 22:56:33 -0500

    mono done, crudely, need to test on live radio

--- src/filter.cpp
+++ src/filter.cpp
@@ -32,18 +32,18 @@ void impulseResponseLPF(float Fs, float Fc, unsigned short int num_taps, std::ve
 void impulseResponseLPF(float Fs, float Fc, unsigned short int num_taps, std::vector<float> &h,const int u)
 {
 	// bring your own functionality
-	h.clear(); h.resize(num_taps, 0.0);
-
+	h.clear(); h.resize(num_taps*u, 0.0);
+	std::cerr << "!!!" << std::endl;
 	float normalized_cutoff = Fc/(u*Fs/2.0);
 
 	for (int i = 0; i < num_taps*u; i++){
 
-		if (i == (num_taps-1.0)/2.0){
-			h[i] = u*normalized_cutoff;
+		if (i == (num_taps*u-1.0)/2.0){
+			h[i] = u*normalized_cutoff; //maybe drop u here
 		} else {
 			h[i] = u*normalized_cutoff*sin(PI*normalized_cutoff*(i-(num_taps-1.0)/2.0))/(PI*normalized_cutoff*(i-(num_taps-1.0)/2.0));
 		}
-		h[i] = h[i]*sin(i*PI/((float)num_taps))*sin(i*PI/((float)num_taps));
+		h[i] = h[i]*sin(i*PI/((float)(u*num_taps)))*sin(i*PI/((float)(u*num_taps)));
 	}
 }
 
@@ -65,6 +65,7 @@ void convolveFIR(std::vector<float> &y, const std::vector<float> &x, const std::
 	initial_state = std::vector<float>(x.end()-h.size()+1,x.end());
 	
 }
+/*
 void convolveFIR(const int over, std::vector<float> &y, const std::vector<float> &x, const std::vector<float> &h, std::vector<float> &initial_state, const int decimation_factor)
 {
 	y.clear(); y.resize(x.size()/decimation_factor);
@@ -81,18 +82,20 @@ void convolveFIR(const int over, std::vector<float> &y, const std::vector<float>
 	initial_state = std::vector<float>(x.end()-h.size()+1,x.end());
 	
 }
-
-void convolveFIR(const int over, std::vector<float> &y, const std::vector<float> &x, const std::vector<float> &h, std::vector<float> &initial_state, const int upsample, const int decimation_factor)
+*/
+void convolveFIR(std::vector<float> &y, const std::vector<float> &x, const std::vector<float> &h, std::vector<float> &initial_state, const int upsample, const int decimation_factor)
 {
-	y.clear(); y.resize(x.size()/decimation_factor);
-	int phase;
-	for (int n = 0; n < x.size(); n+=decimation_factor) {
-		for (int k = 0; k < h.size(); k+=upsample){
-			phase = n % upsample;
-			if (n-k < 0) {
-				y[n/decimation_factor] += (short int)((16384*h[k]*initial_state[n-k+initial_state.size()]));
+	y.clear(); y.resize(x.size()*upsample/decimation_factor);
+	int phase = 0;
+	int x_index = 0;
+	for (int n = 0; n < y.size(); n+=1) { //for each final value in y,
+		phase = (n * decimation_factor) % upsample; // phase to start the convolution
+		for (int k = phase; k < h.size(); k+=upsample){ //only take the filter coeffs from the bank used in current y value
+			x_index = (n*decimation_factor-k)/upsample; //yu = n*decim, xu = yu - k, x = xu / upsample
+			if (x_index < 0) {
+				y[n] += upsample*((h[k]*initial_state[initial_state.size() + x_index])); //index is negative, so this is always valid
 			}else{
-				y[n/decimation_factor] += (short int)((16384*h[k]*x[n-k]));
+				y[n] += upsample*(h[k]*x[x_index]);
 			}
 		}
 	}
